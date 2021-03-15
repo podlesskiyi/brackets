d8c261ae302f4afc4203672f60c045f8651c1329
podlesskiyi <77390001+podlesskiyi@users.noreply.github.com>
2021-03-15 19:33:28 +0300

    docs: update info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let reg = /\(\)|\{\}|\[\]|\|\||[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/;
+
+  while (reg.test(str)) {
+      str = str.replace(reg, '');
+  }
+
+  if (str.length === 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
